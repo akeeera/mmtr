3350a51c5d3020b5321250f0dd260824c02b85c4
welldone <73180631+welldonemixtape@users.noreply.github.com>
2021-12-06 15:23:20 +0300

    переделан жс код
    переделана валидация

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,12 +1,13 @@
-function Complete() {
-    var result =
-        "Name: " + document.application.user_name.value +
-        "\nEmail: " + document.application.user_email.value +
-        "\nText: " + document.application.user_message.value;
-    console.log(result);
-}
+const form = document.forms.application;
 
-function ValidateEmail(email) {
+function complete(event) {
+    event.preventDefault();
+    const name = form.elements.user_name
+    const email = form.elements.user_email
+    const message = form.elements.user_message
+    const nameIsValid = name.value
+    const messageIsValid = message.value.length >= 50
+
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (email.value.match(mailformat)) {
         document.application.user_email.focus();
@@ -14,6 +15,25 @@ function ValidateEmail(email) {
     } else {
         alert("Вы ввели неверный Email");
         document.application.user_email.focus();
-        return false;
+    }
+    if (!nameIsValid) {
+        name.style.borderColor = 'red'
+    } else {
+        name.style.borderColor = 'white'
+    }
+    if (!messageIsValid) {
+        message.value.length < 50
+        message.style.borderColor = 'red'
+    } else {
+        message.style.borderColor = 'white'
+    }
+    if (nameIsValid && messageIsValid) {
+        var result =
+            "Name: " + name.value +
+            "\nEmail: " + name.value +
+            "\nText: " + name.value;
+        console.log(result);
     }
 }
+
+form.addEventListener('submit', complete)
