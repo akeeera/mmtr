18a388149be3369933dac6d84da815a62a27640a
welldone <73180631+welldonemixtape@users.noreply.github.com>
2021-12-09 17:40:12 +0300

    добавил проверку на емеил
    данные не отправятся пока все не будет введено правильно

--- src/js/forms.js
+++ src/js/forms.js
@@ -8,36 +8,37 @@ function complete(event) {
 
     const nameIsValid = name.value
     const messageIsValid = message.value.length >= 50
-    const mailIsValid = email.value.match(mailformat)
 
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.value.match(mailformat)) {
-        form.elements.user_email;
-        return true;
-    } else {
-        alert("Вы ввели неверный Email");
-        form.elements.user_email.focus();
-    }
-    if (!nameIsValid) {
-        name.style.borderColor = 'red'
-    } else {
-        name.style.borderColor = 'white'
-    }
-    if (!messageIsValid) {
-        message.value.length < 50
-        message.style.borderColor = 'red'
-    } else {
-        message.style.borderColor = 'white'
-    }
-    if (!mailIsValid) {
-        message.style.borderColor = 'red'
-    }
-    if (nameIsValid && messageIsValid) {
+    const mailIsValid = email.value.match(mailformat)
+
+    if (nameIsValid && messageIsValid && mailIsValid) {
         var result =
             "Name: " + name.value +
             "\nEmail: " + email.value +
             "\nText: " + message.value;
         console.log(result);
+        alert('Сообщение отправлено');
+    }
+
+    if (email.value.match(mailformat)) {
+        form.elements.user_email;
+        email.style.borderColor = 'white'
+        return true;
+    } else {
+        email.style.borderColor = 'red'
+        alert("Вы ввели неверный Email");
+        form.elements.user_email.focus();
+    }
+    if (!nameIsValid) {
+        name.style.borderColor = 'red';
+    } else {
+        name.style.borderColor = 'white';
+    }
+    if (!messageIsValid) {
+        message.style.borderColor = 'red';
+    } else {
+        message.style.borderColor = 'white';
     }
 }
 
