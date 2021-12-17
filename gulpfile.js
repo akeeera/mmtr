dc0b86322096a2378ce53f6566a75b4ee5ed4342
welldone <73180631+welldonemixtape@users.noreply.github.com>
2021-12-17 14:38:24 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -22,16 +22,26 @@ gulp.task('browser-sync', function () { // Создаем таск browser-sync
     });
 });
 
-gulp.task('scripts', function() {
-	return gulp.src([ // Берем все необходимые библиотеки
-		'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
-		])
-		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
-		.pipe(uglify()) // Сжимаем JS файл
-		.pipe(gulp.dest('src/js')); // Выгружаем в папку src/js
+gulp.task('scripts', function () {
+    return gulp.src([ // Берем все необходимые библиотеки
+            'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
+            'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('src/js')); // Выгружаем в папку src/js
+});
+
+gulp.task('code', function () {
+    return gulp.src('src/*.html')
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 gulp.task('watch', function () {
     gulp.watch('src/sass/**/*.sass', gulp.parallel('sass')); // Наблюдение за sass файлами
+    gulp.watch('src/*.html', gulp.parallel('code')); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch(['src/js/forms.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
 });
+gulp.task('default', gulp.parallel('sass', 'scripts', 'browser-sync', 'watch'));
